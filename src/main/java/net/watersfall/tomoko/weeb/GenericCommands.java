d17eca955bc02ce4aef115a1a51665d520b1eabd
Watersfall <dontshoot262@yahoo.com>
2021-04-21 13:53:43 -0400

    No embeds :pensive:

--- src/main/java/net/watersfall/tomoko/weeb/GenericCommands.java
+++ src/main/java/net/watersfall/tomoko/weeb/GenericCommands.java
@@ -26,12 +26,12 @@ public class GenericCommands
 				if(args.length == 0)
 				{
 					int random = (int)(Math.random() * smugs.size());
-					event.getChannel().sendMessage(EmbedUtils.getImageEmbed("Smug #" + random, smugs.get(random)));
+					event.getChannel().sendMessage("Smug #" + random + "\n" + smugs.get(random));
 				}
 				else if(args.length == 1 && args[0].matches("-?\\d+"))
 				{
 					int index = Integer.parseInt(args[0]);
-					event.getChannel().sendMessage(EmbedUtils.getImageEmbed("Smug #" + index, smugs.get(index)));
+					event.getChannel().sendMessage("Smug #" + index + "\n" + smugs.get(index));
 				}
 			}
 		}
